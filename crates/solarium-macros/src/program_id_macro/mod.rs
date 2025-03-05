3f8556164e5b8aa335769eec5b15b865ca6a215d
Danilo Guanabara <danilo@magicblock.gg>
2025-03-05 02:25:46 -0300

    :recycle: Using proper API to get Program from Workspace

--- crates/solarium-macros/src/program_id_macro/mod.rs
+++ crates/solarium-macros/src/program_id_macro/mod.rs
@@ -4,9 +4,8 @@ use solarium_workspace::Workspace;
 use anyhow::Result;
 pub fn process(program_name: &str) -> Result<TokenStream> {
     let workspace = Workspace::current()?;
-    let program = workspace.programs.iter().find(|p| p.name == program_name).ok_or(anyhow::anyhow!("Program {} not found", program_name))?;
-    let program_id = program.public_key;
-    let program_id = program_id.to_bytes();
+    let program = workspace.program(program_name).ok_or(anyhow::anyhow!("Program {} not found", program_name))?;
+    let program_id = program.public_key.to_bytes();
 
     let expanded = quote! {
         ::solana_program::pubkey::Pubkey::new_from_array([
