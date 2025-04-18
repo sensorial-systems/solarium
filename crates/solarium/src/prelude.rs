7722c8825ef3bf63bde6a9cb3d70461af208fca4
Danilo Guanabara <danilo@magicblock.gg>
2025-04-18 03:02:11 -0300

    :recycle: Re-exporting traits in prelude

--- crates/solarium/src/prelude.rs
+++ crates/solarium/src/prelude.rs
@@ -2,3 +2,6 @@ pub use solarium_macros::*;
 pub use crate::result::*;
 pub use solana_program;
 pub use borsh;
+
+pub use crate::{Pda, Space, Owner, AccountInitialization, AccountUpdate};
+pub use solana_program::pubkey::Pubkey;
