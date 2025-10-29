de22031f9b2637a35af51ca94f3deda1e7a1de95
Danilo Guanabara <danilo@sensorial.systems>
2025-10-29 05:23:09 -0300

    feat: Adding fn transfer to Signer

--- crates/solarium-program/src/signer/mod.rs
+++ crates/solarium-program/src/signer/mod.rs
@@ -1,11 +1,29 @@
+use crate::Account;
 use crate::prelude::*;
 use crate::Check;
+use solana_program::program::invoke;
 use solana_program::{account_info::AccountInfo, program_error::ProgramError};
 
 #[derive(Clone, Copy)]
 pub struct Signer<'a> {
     /// The account info.
     pub info: &'a AccountInfo<'a>,
+}
+
+impl<'a> Signer<'a> {
+    pub fn address(&self) -> Pubkey {
+        *self.info.key
+    }
+
+    pub fn transfer<T>(&self, amount: u64, to: &Account<'a, T>) -> Result<()> {
+        let instruction = solana_program::system_instruction::transfer(
+            &self.info.key,
+            &to.info.key,
+            amount,
+        );
+        invoke(&instruction, &[self.info.clone(), to.info.clone()])?;
+        Ok(())
+    }
 }
 
 impl<'a> TryFrom<&'a AccountInfo<'a>> for Signer<'a> {
